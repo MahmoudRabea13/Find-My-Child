3d2da6d86a1dc0eb4c25ab83e8042ae96a56d131
unknown <mahmoudrabia425@gmail.com>
2023-05-11 18:24:48 +0300

    FaceDetection

---
+++ FaceDetection.py
@@ -0,0 +1,25 @@
+import cv2 
+class FaceDetection():
+    def detect_faces(self,image_path):
+        # Load the image
+        img = cv2.imread(image_path)
+        # Convert the image to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Load the face detection classifier
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        # Detect faces in the image
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=3, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+        Dimensions=[]
+        # Loop through the faces and draw rectangles around them
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            face= [x,y,w,h]
+            Dimensions.append(face)
+        cv2.imwrite('Detected.jpg',img)
+        return len(faces),Dimensions
+img = FaceDetection.detect_faces(FaceDetection,'./Faces/Mahmoud Rabea.jpg')
+
+#print(img[0]) # return the number of detected faces 
+#print(img[1]) #list of lists of dimensions of each detected face [x,y,w,h]
+#print(img[1][0]) #the dimensions of the first face detected
+#print(img[1][0][1]) # y of first face , likewise
