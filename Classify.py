94e3f47f50f09edbb3b37d76d4231f3050ecd9cb
Mohamed Nasser <nassermohamed3222@gmail.com>
2023-05-18 02:32:28 +0300

    added svm

--- Classify.py
+++ Classify.py
@@ -1,30 +1,37 @@
 import cv2
 import numpy as np
+
+
 class Classify():
     def __init__(self):
         self.means = 0
-        self.number = 5
-    def fit(self,X,y,number):
-        M,N = X.shape
-        means = np.zeros((number,N))
+        self.number = 3
+
+    def fit(self, X, y, number):
+        M, N = X.shape
+        means = np.zeros((number, N))
         for i in range(number):
-            means[i,:] = (1/N) * np.sum(X[y == i,: ], axis = 0 )
+            means[i, :] = (1/N) * np.sum(X[y == i, :], axis=0)
         self.means = means
         self.number = number
-    def distance(self,X1,X2):
+
+    def distance(self, X1, X2):
         diff = X1-X2
-        d = np.sqrt(np.dot(diff,diff))
+        d = np.sqrt(np.dot(diff, diff))
         return d
-    def predict(self,test_img):
+
+    def predict(self, test_img):
         min_dist = np.inf
         min_class = -1
         for i in range(self.number):
-            d = self.distance(test_img, self.means[i,:])
+            d = self.distance(test_img, self.means[i, :])
             if d < min_dist:
                 min_dist = d
-                min_class = i 
+                min_class = i
         return min_class
+
     def save_weights(self):
-        np.save('Weights',self.means)
+        np.save('Weights', self.means)
+
     def load_weights(self):
         self.means = np.load('./Weights.npy')
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template,request , jsonify , json
-import cv2 
+from flask import Flask, render_template, request, jsonify, json
+import cv2
 import numpy as np
 from FaceDetection import FaceDetection
 from Classify import Classify
@@ -9,41 +9,47 @@ Face = FaceDetection()
 Model = Classify()
 pca = NPCA()
 Face = FaceDetection()
+
+
 @app.route('/')
 def main():
     Model.load_weights()
     pca.load_preprocessing()
     return render_template('index.html')
-@app.route('/input' , methods = ['POST', 'GET'] )
+
+
+@app.route('/input', methods=['POST', 'GET'])
 def input():
     if request.method == 'POST':
         img = request.files.get('input_img')
         name = './static/Imgs/' + img.filename + '.jpg'
         img.save(name)
-        X_try = cv2.imread('./static/imgs/input_img.jpg',0)
-        n,dim = Face.detect_faces('./static/imgs/input_img.jpg')
-        names = ['Ahmed','Rabea','Nasser','Dina','Abdlerhman']
+        X_try = cv2.imread('./static/imgs/input_img.jpg', 0)
+        n, dim = Face.detect_faces('./static/imgs/input_img.jpg')
+        names = ['Rabea', 'Nasser', 'Abdelrhman']
         imgs_n = []
         pred = []
         for i in range(n):
-            new_img = X_try[dim[i][1]:dim[i][1]+dim[i][3],dim[i][0]:dim[i][0]+dim[i][2]]
-            new_img = cv2.resize(new_img,(64,64))
+            new_img = X_try[dim[i][1]:dim[i][1]+dim[i]
+                            [3], dim[i][0]:dim[i][0]+dim[i][2]]
+            new_img = cv2.resize(new_img, (64, 64))
             # cv2.imwrite('./static/Imgs/output_img.jpg',new_img)
             imgs_n.append(new_img)
         imgs_n = np.array(imgs_n)
         ll = pca.preprocess_data(imgs_n)
         ll = pca.reduce_dim(ll)
         for i in range(n):
-            y_hat = Model.predict(ll[i,:])
-            pred.append(names[y_hat]) 
+            y_hat = Model.predict(ll[i, :])
+            pred.append(names[y_hat])
         # print(pred)
         # print(n)
         Face.save(pred)
-        result = [n ,pred]
+        result = [n, pred]
         # print(result)
         return jsonify(result)
     else:
         return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
